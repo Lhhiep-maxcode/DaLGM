d1d8e6fd1673831a58ebbd00443837bc6ecc4f5e
Lhhiep-maxcode <22028295@vnu.edu.vn>
2026-05-24 23:35:29 +0700

    update inference for PALGM

--- 3Dreconstruct_infer.py
+++ 3Dreconstruct_infer.py
@@ -61,15 +61,28 @@ proj_matrix[2, 2] = (cfg.zfar + cfg.znear) / (cfg.zfar - cfg.znear)
 proj_matrix[3, 2] = - (cfg.zfar * cfg.znear) / (cfg.zfar - cfg.znear)
 proj_matrix[2, 3] = 1
 
-cam_input_config = {
-    0: [0, 0],
-    2: [0, 90],
-    4: [0, 180],
-    6: [0, 270],
-    24: [89.89, 180]
-}
+# # ===== cultural-dataset inference =====
+# cam_input_config = {
+#     1: [0, 0],
+#     2: [0, 45],
+#     3: [0, 90],
+#     4: [0, 135],
+#     5: [0, 180],
+#     6: [0, 225],
+#     7: [0, 270],
+#     8: [0, 315],
+#     9: [89.9, 180]
+# }
+# view_ids = [1, 3, 5, 7, 9]
 
-view_ids = [0, 2, 4, 6, 24]
+# ===== benchmark inference =====
+cam_input_config = {
+    **{i: [0, 360 / 64 * i] for i in range(cfg.num_views_total - 1)},
+    64: [89.89, 180],
+}
+view_ids = [0, 8, 16, 24, 32, 40, 48, 56, 64]
+
+
 
 def find_nonzero_bbox(alpha_channel):
     """Find bounding box (ymin, ymax, xmin, xmax) where alpha > 0."""
@@ -88,7 +101,7 @@ def run(cfg: Options, path):
     global_ymin, global_ymax = 1e9, -1
     global_xmin, global_xmax = 1e9, -1
     for view_id in view_ids:
-        image_path = os.path.join(path, 'rgb', f'{view_id:03d}.png')
+        image_path = os.path.join(path, f'{view_id:03d}.png')
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)  # shape: [512, 512, 4]
         alpha = image[:, :, 3]
         
@@ -128,15 +141,29 @@ def run(cfg: Options, path):
                 for image in images]
     masks = [mask[min_res:(origin_size - min_res), min_res:(origin_size - min_res)]
                 for mask in masks]
-    
+
+    # adaptive input
+    if cfg.pixel_align:
+        num_views_actual = len(images)
+        print("num_views_actual", num_views_actual)
+        print("cfg.num_views_input", cfg.num_views_input)
+        if num_views_actual < cfg.num_views_input:
+            pad_n = cfg.num_views_input - num_views_actual
+            images    = images    + images[-pad_n:]
+            masks     = masks     + masks[-pad_n:]
+            cam_poses = cam_poses + cam_poses[-pad_n:]
+        
     images = torch.stack(images, dim=0)     # [V, C, H, W]
+    print("Input images num:", images.shape[0])
     masks = torch.stack(masks, dim=0)       # [V, H, W]
+    print("Input masks num:", masks.shape[0])
     cam_poses = torch.stack(cam_poses, dim=0)  # [V, 4, 4]
+    print("Input poses num:", cam_poses.shape[0])
     images = F.interpolate(images, size=(cfg.input_size, cfg.input_size), mode='bilinear', align_corners=False)
     images = TF.normalize(images, IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
 
     rays_embeddings = []
-    for i in range(len(view_ids)):
+    for i in range(cfg.num_views_input):
         rays_o, rays_d = get_rays(cam_poses[i], cfg.input_size, cfg.input_size, cfg.fovy) # [h, w, 3]
         rays_plucker = torch.cat([torch.cross(rays_o, rays_d, dim=-1), rays_d], dim=-1) # [h, w, 6]
         rays_embeddings.append(rays_plucker)
@@ -148,11 +175,33 @@ def run(cfg: Options, path):
     with torch.no_grad():
         input_images = input_images.unsqueeze(0).to(device) # [1, V, 9, H, W]
         gaussians = model.forward_gaussians(input_images)
+        
+        if cfg.pixel_align:
+            rays_d_list, rays_o_list = [], []
+            cam_poses_input_tensor = cam_poses.to(device)  # [V, 4, 4] - the input view poses you built earlier
+            for i in range(cfg.num_views_input):
+                ro, rd = get_rays(cam_poses_input_tensor[i], cfg.splat_size, cfg.splat_size, cfg.fovy)
+                rays_d_list.append(rd)
+                rays_o_list.append(ro)
+            
+            rays_d = torch.stack(rays_d_list, dim=0).to(device)  # [V, h, w, 3]
+            rays_o = torch.stack(rays_o_list, dim=0).to(device)  # [V, h, w, 3]
+    
+            pos = gaussians[..., 0:3]  # [1, V*h*w, 3]
+            dist = pos.mean(dim=-1, keepdim=True).sigmoid() * cfg.max_distance
+            pos = dist * rays_d.view(1, -1, 3) + rays_o.view(1, -1, 3)
+            gaussians = torch.cat([pos, gaussians[..., 3:]], dim=-1)  # [1, N, 14]
 
         model.gs.save_ply(gaussians, os.path.join(cfg.workspace, 'result.ply'))
+        gaussians_filtered = model.gs.load_ply(os.path.join(cfg.workspace, 'result.ply'))  # [N, 14] cpu tensor
+        alpha_mask = gaussians_filtered[:, 3] > 0.004
+        gaussians_filtered = gaussians_filtered[alpha_mask]
+        gaussians_filtered = gaussians_filtered.unsqueeze(0)  # [1, N, 14]
+        model.gs.save_ply(gaussians_filtered, os.path.join(cfg.workspace, 'result.ply'))
+        gaussians = gaussians_filtered.to(device)
 
         images = []
-        elevation = 30
+        elevation = 0
         azimuth = np.arange(0, 720, 4, dtype=np.int32)
 
         for azi in tqdm(azimuth):
@@ -171,7 +220,7 @@ def run(cfg: Options, path):
             if cfg.fancy_video:
                 scale = min(azi / 720, 1)
             else:
-                scale = 1
+                scale = 1.6
 
             image = model.gs.render(gaussians, cam_view.unsqueeze(0), cam_view_proj.unsqueeze(0), cam_pos.unsqueeze(0), scale_modifier=scale)['image']
             images.append((image.squeeze(1).permute(0,2,3,1).contiguous().float().cpu().numpy() * 255).astype(np.uint8))
@@ -180,5 +229,5 @@ def run(cfg: Options, path):
         imageio.mimwrite(os.path.join(cfg.workspace, path.split('/')[-1] + '.mp4'), images, fps=30)
         
 
-path = '/kaggle/input/10k-objaverse-object/archive_10/003d816301084f719897503bdb4ffbc3'
+path = '/kaggle/input/datasets/laihoanghiep/100-abo-rgb-input/archive_001/B01D7P5BFS/rgb'
 run(cfg, path)
