43091600f75d21ca023a638b9a29860ce7d81250
Lhhiep-maxcode <22028295@vnu.edu.vn>
2026-05-31 10:15:15 +0700

    update README.md

--- README.md
+++ README.md
@@ -43,9 +43,11 @@ dataset_root/
 ├── archive_001/
 │   └── object_name/
 │       ├── rgb/
-│       │   ├── 000.png
-│       │   ├── 001.png
-│       │   └── ...     
+│       │   ├── 000.png (elev: 0, azim: 0)
+│       │   ├── 001.png (elev: 0, azim: 5.625)
+|       |   |
+│       │   └── 063.png (elev: 0, azim: 354.375) 
+│       │   └── 064.png (elev: 90, azim: 180)     
 │       └── depth/
 │           ├── 000.npz
 │           └── ...
@@ -65,14 +67,19 @@ Or manually:
 
 ```bash
 accelerate launch --config_file accelerate_configs/gpu2.yaml main.py big \
-    --resume pretrained/model_fp16_fixrot.safetensors --fine_tune \
+    --resume /workspace/LGM-from-sratch/best_phase1/best_phase1_model.safetensors --fine_tune \
     --workspace workspace \
     --data_path /path/to/dataset \
     --depth1_path /path/to/depth_dataset \
-    --lambda_depth 0.5 --depth_loss_type l1 \
-    --pixel_align --input_size 160 --splat_size 160 \
-    --num_views_input 9 --num_views_output 9 \
-    --batch_size 6 --num_epochs 50 --lr 1e-4
+    --lambda_depth 0.5 --lambda_depth_rank 0.3 --depth_loss_type l1 \
+    --batch_size 6 --mixed_precision fp16 --input_size 160 --splat_size 160 --pixel_align \
+    --output_size 512 --num_epochs 50 --train_size 0.8 --num_views_input 9 --num_views_output 9 \
+    --lr 1e-4 --gradient_accumulation_steps 4 --warmup_steps 2500 \
+    --wandb_project_name YOUR_PROJECT_NAME \
+    --wandb_experiment_id YOUR_EXPERIMENT_ID \
+    --wandb_experiment_name YOUR_EXPERIMENT_NAME \
+    --wandb_key YOUR_WANDB_KEY \
+    > train.log 2>&1 &
 ```
 
 ---
--- train.sh
+++ train.sh
@@ -1,18 +1,14 @@
 nohup accelerate launch --config_file accelerate_configs/gpu2.yaml main.py big \
     --resume /workspace/LGM-from-sratch/best_phase1/best_phase1_model.safetensors --fine_tune \
-    --workspace workspace --data_path /workspace/10k-dataset-9-views \
-    --depth1_path /workspace/10k-dataset-9-views \
-    --depth2_path None \
-    --depth3_path None \
-    --depth4_path None \
-    --lambda_depth 0.5 --lambda_grad -1 --lambda_opacity -1 --depth_loss_type l1 \
-    --lambda_mse_start 1.0 --lambda_mse_end 1.0 \
-    --lambda_lpips_start 1.0 --lambda_lpips_end 1.0 \
-    --num_workers 4 --batch_size 6 --mixed_precision fp16 --input_size 160 --splat_size 160 --pixel_align \
+    --workspace workspace \
+    --data_path /path/to/dataset \
+    --depth1_path /path/to/depth_dataset \
+    --lambda_depth 0.5 --lambda_depth_rank 0.3 --depth_loss_type l1 \
+    --batch_size 6 --mixed_precision fp16 --input_size 160 --splat_size 160 --pixel_align \
     --output_size 512 --num_epochs 50 --train_size 0.8 --num_views_input 9 --num_views_output 9 \
-    --alpha_threshold 0.004 --distance_threshold -1 --scale_threshold -1 --rot_threshold -1 --rgb_threshold -1 \
-    --lr 1e-4 --gradient_accumulation_steps 4 --warmup_steps 10000 \
-    --wandb_project_name LGM_4001 --wandb_experiment_id None \
-    --wandb_experiment_name adaptive_LGM-with-depthlossL1-no_grad-pruning \
-    --wandb_key 2643e7f5dd32fdc64ae63918abf4238ad72c0d60 \
+    --lr 1e-4 --gradient_accumulation_steps 4 --warmup_steps 2500 \
+    --wandb_project_name YOUR_PROJECT_NAME \
+    --wandb_experiment_id YOUR_EXPERIMENT_ID \
+    --wandb_experiment_name YOUR_EXPERIMENT_NAME \
+    --wandb_key YOUR_WANDB_KEY \
     > train.log 2>&1 &
