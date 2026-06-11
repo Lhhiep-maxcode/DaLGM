da7d0d999e6d04510c2f3fbb869fa345f0ba473e
Lhhiep-maxcode <22028295@vnu.edu.vn>
2026-06-11 23:29:07 +0700

    change the datapath in train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 nohup accelerate launch --config_file accelerate_configs/gpu2.yaml main.py big \
     --resume best_phase1/best_phase1_model.safetensors --fine_tune \
     --workspace workspace \
-    --data_path ../10k-dataset-9-views \
-    --depth1_path ../10k-dataset-9-views \
+    --data_path 10k-dataset-9-views \
+    --depth1_path 10k-dataset-9-views \
     --lambda_depth 0.5 --lambda_grad -1 --lambda_opacity -1 --lambda_depth_rank 0.3 --depth_loss_type l1 \
     --num_workers 4 --batch_size 6 --mixed_precision fp16 --input_size 160 --splat_size 160 --pixel_align \
     --output_size 512 --num_epochs 50 --train_size 0.8 --num_views_input 9 --num_views_output 9 \
